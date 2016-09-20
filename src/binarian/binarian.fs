181e6d634974b1c798798cfe6762f5c7c3c25f4e
Ross Donaldson <gastove@gmail.com>
2016-09-20 15:30:52 -0700

    Clean up formats, read poemmmm

--- src/binarian/binarian.fs
+++ src/binarian/binarian.fs
@@ -1,15 +1,16 @@
 module Binarian
 
+open System.Drawing
 
 let binarateInt (i: int) =
     System.Convert.ToString(i, 2)
-
 
 let binarateStr (s: string) =
     s
     |> Seq.map (int >> binarateInt)
     |> System.String.Concat
-
+    |> Seq.map System.Char.GetNumericValue
+    |> Seq.map int
 
 let binarate strArray =
     strArray
@@ -19,13 +20,21 @@ let binarate strArray =
 
 let binarateFile path =
     System.IO.File.ReadAllLines(path)
-
+    |> Seq.map binarateStr
 
 [<EntryPoint>]
 let main argv =
-    let p = @"/Users/gastove/Documents/bonne_journee.txt"
-    let poem = binarateFile p
-    let res = Seq.map binarateStr poem
-    for line in res do
-        printfn "%A" line
+    let poemPath = @"/Users/gastove/Documents/bonne_journee.txt"
+    let imagePath = @"/Users/gastove/Pictures/bonne_journee.png"
+    let binaryPoem = binarateFile poemPath
+
+    let border = 10
+    let cellSize = 25
+    let gridLineWidth = 1
+    let backgroundColor = Color.BurlyWood
+    let lineColor = Color.BlanchedAlmond
+    let zeroColor = Color.Black
+    let oneColor = Color.White
+    Empattern.drawPattern imagePath binaryPoem border cellSize gridLineWidth backgroundColor lineColor zeroColor oneColor
+
     0 // return an integer exit code
